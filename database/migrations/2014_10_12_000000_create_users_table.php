1286101f47e4f3e6d207c6fdb073031659464f33
Achraf Bchir <achraf.achraf.ba@gmail.com>
2020-12-13 15:13:47 +0100

    add passport package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_email_verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('profil_picture')->nullable();
+            $table->text('actuality')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
